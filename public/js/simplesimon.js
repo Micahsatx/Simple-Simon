97da8f95c27612f718a8d76e0fb651ab58182afc
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-07 14:17:05 -0500

    have randomizer.  working on making functions to light up buttons squencially

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -27,38 +27,51 @@
 
 
 
-    var getColorButton = document.getElementsByClassName("color-button");
-    console.log(getColorButton);
+    // this is the array that varies in length!  holds the random blocks
+    var colorSequence = [];
+    var index = 0;
 
-    // find a random button
-    var keys = $('.color-button');
-    var random = keys[Math.floor(Math.random()*4)];
+    function lightUpANewButton(){
+        // find a random button
+        var keys = $('.color-button');
+        var randomButtonLit = keys[Math.floor(Math.random()*keys.length)];
 
-    $("#idOfStart").click(function(){
-        $(random).fadeTo("fast", 0.4);
-        $(random).fadeTo("fast", 1);
-    });
-    console.log(random);
-
-
-    // $(document).click(function(event){
-    //         console.log(event.keyCode);
-
-    //         if (the array is the same [index] == event.keyCode){
-    //             index++;
-    //         } else {
-    //             // this failed function!
-    //             index = 0;
-    //         }
-
-    //         if (index == cheat.length) {
-    //             index = 0;
-    //             // this suceeded function!!
-    //         }
-        
+        // click listener for start button
+        $("#idOfStart").click(function(){
+            $(randomButtonLit).fadeTo("fast", 0.4);
+            $(randomButtonLit).fadeTo("fast", 1);
+        });
+        // which button was lit?
+        console.log(randomButtonLit);
+    
+        // this is pushing the randomButtonLit onto the color sequence array
+        colorSequence.push(randomButtonLit)
+        console.log(colorSequence)
+    };
 
 
-    // });
+    $(".color-button").click(function(event){
+            console.log(event.randomButtonLit);
+            console.log(this);
+
+            if ( colorSequence[index] == this ){
+                console.log("correct");
+                index++;
+                lightUpANewButton();
+            } else {
+                // this failed function!
+                index = 0;
+            }
+
+            if (index == colorSequence.length) {
+                index = 0;
+                console.log("This is the correct Sequence.")
+                // this success function
+            }
+            
+
+
+        });
 
 
 
