9b1ef768fc2e8018e91378fb699ce8dd619e1c82
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-08 13:25:27 -0500

    added minor styling.  got buttons into a circle.  functionality working.

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -43,25 +43,27 @@
         // this is pushing the randomButtonLit onto the color sequence array
         colorSequence.push(randomButtonLit)
         console.log(colorSequence)
-// i thought calling the function would then trigger lighting up a button.  it does not.
+        // start the animate sequence...simons turn.
         animateSequence();
     };
 
+    // this function makes button light up/ fade out.  thats it.  calling this function 
+    // animateSequence();
     function lightUpSingleButton(button) {
         // use $button to fadeTo 0.4 then 1
         $(button).fadeTo("slow", 0.2);
         $(button).fadeTo("slow", 1);
-// this function makes button light up/ fade out.  thats it.  calling this function 
-// animateSequence();
-
+        // animateSequence();
     }
 
 
+    //this carries a mini function inside of it, because it cannot retrieve the information
+    // from within another function.  so we can give it the information from lightUpSingleButton and tell
+    // it the index to start at then continue to the next index.
     function animateSequence() {
-        //this carries a mini function inside of it, because it cannot retrieve the information
-        // from within another function.  so we can give it the information from lightUpSingleButton and tell
-        // it the index to start at then continue to the next index.
-        
+        // variable that only exists within the function. if the lightUpSingleButton is equal to the arrays
+        // index then increase the index by 1.  clear the interval and set the index back to zero for the users
+        // turn
         var simonSequenceToLightUp = setInterval(function(){
             lightUpSingleButton(colorSequence[index]);
             index++;
@@ -70,10 +72,11 @@
                 clearInterval(simonSequenceToLightUp);
                 index = 0;
             }
+        // here is the delay. 1.5 seconds
         }, 1500);
     }
 
-// tried making this start click trigger the sequence...it does not.
+
     //click listener for start button
     $("#idOfStart").click(function(){
         newRandomButton();
@@ -84,8 +87,6 @@
 
     // the part that checks to see if the button clicked is equal to the button that lit up.
     $(".color-button").click(function(event){
-            console.log(colorSequence[index])
-            console.log(this);
 
             if ( colorSequence[index] == this ){
                 console.log("correct");
@@ -101,11 +102,9 @@
                 console.log("This is the correct Sequence of buttons!")
                 newRandomButton();
                 // this success function
-            }
-            
-
-
-        });
+                $("stage").html("level " + (index + 1));
+            }    
+    });
 
 
 
